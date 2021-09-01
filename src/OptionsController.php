8d20c898fb549452100331227a8d55aeec7d05b0
parth-ka-patel <parth.kapatel@arsenaltech.com>
2021-09-01 18:43:22 +0530

    Use for Nova Tabs also

--- src/OptionsController.php
+++ src/OptionsController.php
@@ -4,7 +4,6 @@ namespace Alvinhu\ChildSelect;
 
 use Illuminate\Routing\Controller;
 use Laravel\Nova\Http\Requests\NovaRequest;
-use function Couchbase\defaultDecoder;
 
 class OptionsController extends Controller
 {
@@ -15,22 +14,9 @@ class OptionsController extends Controller
 
         $resource = $request->newResource();
         $fields = $resource->updateFields($request);
-        $field = $this->findFieldByAttribute($fields,$attribute);
+        $field = $fields->findFieldByAttribute($attribute);
         $options = $field->getOptions($parentValue);
 
         return $options;
-    }
-
-    public function findFieldByAttribute($field,$attribute){
-        return array_filter($field,function ($field) use ($attribute) {
-            if(gettype($field) == "array" && $field["component"] == "tabs"){
-                array_filter($field["fields"],function ($field) use($attribute){
-                    return isset($field->attribute) && $field->attribute = $attribute;
-                });
-            }else{
-                return isset($field->attribute) &&
-                    $field->attribute == $attribute;
-            }
-        });
     }
 }
